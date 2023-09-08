f65be755939f9384a7c1ccc9fd7a33a5d326a49c
Abod Michael <abodmicheal@gmail.com>
2023-09-08 05:32:44 -0500

    update to the index
    des

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
-// TypingText.js
+// TypingText
 const TypingText = ({
   textArray,
   typingSpeed,
